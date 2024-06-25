054823d3ed5fd0c30d329e474e9d1f62729fbda0
yourfavoritemustache <simoasto95@gmail.com>
2024-06-25 12:17:04 +0200

    Query 6th commit

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -6,6 +6,7 @@ Answer: What are the most optimal skills to learn (aka it’s in high demand and
     offering strategic insights for career development in data analysis
 */
 
+--Runtime 00:00:00.506
 WITH skills_demand AS (
     SELECT 
         skills_dim.skill_id,
@@ -40,3 +41,27 @@ SELECT
 FROM
     skills_demand
 INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+ORDER BY 
+    demand_count DESC,
+    avg_salary DESC
+LIMIT 15
+
+
+-- --Compact code version runtime 00:00:00.186
+-- SELECT
+--     skills_dim.skill_id,
+--     skills_dim.skills,
+--     COUNT(job_postings_fact.job_id) AS demand_count,
+--     ROUND(AVG(salary_year_avg),0) AS avg_salary
+-- FROM job_postings_fact
+-- INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+-- INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+-- WHERE 
+--     job_title_short = 'Data Analyst' AND
+--     job_location LIKE '%Netherlands' AND
+--     salary_year_avg IS NOT NULL
+-- GROUP BY skills_dim.skill_id
+-- ORDER BY 
+--     demand_count DESC,
+--     avg_salary DESC
+-- LIMIT 15
